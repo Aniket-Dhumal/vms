bd6c645179b0734ceadda6c2873ff80ff1a23cf9
Aniket-Dhummal <dhumalhari1410@gmail.com>
2023-10-19 23:12:28 +0530

    Dakhale

--- application/views/include/menu.php
+++ application/views/include/menu.php
@@ -95,6 +95,17 @@
 
 							</ul>
 						</li>
+						<li class="slide">
+							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon fa fa-cogs"></i><span class="side-menu__label">नोंदणी करा.</span><i class="angle fa fa-angle-right"></i></a>
+							<ul class="slide-menu">
+						
+							   <li><a href="<?=base_url();?>Admin/BirthRegistration" class="slide-item">कुटुंब नोंदणी.</a></li>
+								<li><a href="<?=base_url();?>Admin/BirthRegistration" class="slide-item">जन्म नोंदणी.</a></li>
+								<li><a href="<?=base_url();?>Admin/DeathRegistration" class="slide-item">मृत्यू नोंदणी.</a></li>
+								<li><a href="<?=base_url();?>Admin/MerriageRegistration" class="slide-item">विवाह नोंदणी.</a></li>
+
+							</ul>
+						</li>
 
                   <li class="slide">
 							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon fa fa-cogs"></i><span class="side-menu__label">कर संकलन.</span><i class="angle fa fa-angle-right"></i></a>
